625ab47dd89a275370f0da65726a743dc3c4d5ba
Juan Manuel Carranza <ikkicarranza@gmail.com>
2023-09-01 17:27:53 -0400

    useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+    const onLogout = ()=>{
+        navigate('/login', {
+            replace: true
+        });
+    }
+    
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -43,7 +50,9 @@ export const Navbar = () => {
                         Ikki
                         </span>
                     <button
-                        className='nav-item nav-link btn'>
+                        className='nav-item nav-link btn'
+                        onClick={onLogout}
+                        >
                             Logout
                         </button>
                 </ul>
