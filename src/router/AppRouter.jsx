9e6153b6a3e4de11bbb1f0895cdc3c88e65d2aed
Juan Manuel Carranza <ikkicarranza@gmail.com>
2023-09-01 16:59:25 -0400

    Se agrego router secundario

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-import { MarvelPage, DcPage  } from "../heroes"
+import {  HeroesRoutes  } from "../heroes"
 import { LoginPage } from "../auth/pages/LoginPage"
-import { Navbar } from "../ui"
+
 
 
 export const AppRouter = () => {
   return (
    <>
-   <Navbar/>
+
    <Routes>
-    <Route path="marvel" element={<MarvelPage />}/>
-    <Route path="dc" element={<DcPage />}/>
+
     <Route path="login" element={<LoginPage />}/>
-    <Route path="/" element={<Navigate to="/Dc" />}/>
+
+    <Route path="/*" element={<HeroesRoutes/>}/>
+
    </Routes>
    </>
   )
