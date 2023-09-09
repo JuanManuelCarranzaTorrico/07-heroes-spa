41228bd31fa8c06996b6bc4aa6544cb2b196d7d3
Juan Manuel Carranza <ikkicarranza@gmail.com>
2023-09-09 11:38:17 -0400

    Add Auth Context

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,17 @@
+import { Children, useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./authReducer"
+
+
+export const AuthProvider = ({ children }) => {
+const initialState = {
+    logged: false
+}
+    const [authState, dispatch ]=useReducer( authReducer, initialState  )
+
+  return (
+    <AuthContext.Provider value={{}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
